1de92b3a8bac69593dc5ed7833df8a9c58a3460c
ChenShu23 <124551913+ChenShu23@users.noreply.github.com>
2023-07-19 01:07:01 +0800

    Create attention_Layer.py

---
+++ model/layer/attention_Layer.py
@@ -0,0 +1,113 @@
+import torch.nn as nn
+import torch
+from math import sqrt
+
+class Channel_AttentionLayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(Channel_AttentionLayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+                nn.Linear(channel, channel // reduction),
+                nn.ReLU(inplace=True),
+                nn.Linear(channel // reduction, channel),
+                nn.Sigmoid()
+                )
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)   # (20, 30)
+        y = self.fc(y)   #(20, 30)
+        y = y.view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+class AttentionLayer(nn.Module):
+    def __init__(self, channel, reduction=1, multiply=True):
+        super(AttentionLayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel),
+            nn.Sigmoid()
+        )
+        self.multiply = multiply
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        if self.multiply == True:
+            return x * y
+        else:
+            return y
+
+
+class SpatialSELayer(nn.Module):
+    """
+    Re-implementation of SE block -- squeezing spatially and exciting channel-wise described in:
+        *Roy et al., Concurrent Spatial and Channel Squeeze & Excitation in Fully Convolutional Networks, MICCAI 2018*
+    """
+
+    def __init__(self, num_channels):
+        """
+        :param num_channels: No of input channels
+        """
+        super(SpatialSELayer, self).__init__()
+        self.conv = nn.Conv2d(num_channels, 1, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, input_tensor):
+        """
+        :param input_tensor: X, shape = (batch_size, num_channels, H, W)
+        :return: output_tensor
+        """
+        # spatial squeeze
+        batch_size, _, a, b = input_tensor.size()
+        squeeze_tensor = self.sigmoid(self.conv(input_tensor))
+
+        # spatial excitation
+        output_tensor = torch.mul(input_tensor, squeeze_tensor.view(batch_size, 1, a, b))
+
+        return output_tensor
+
+
+class CALayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(CALayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool3d(1)
+        self.conv_du = nn.Sequential(
+                nn.Conv3d(channel, channel // reduction, 1, padding=0, bias=True),
+                nn.ReLU(inplace=True),
+                nn.Conv3d(channel // reduction, channel, 1, padding=0, bias=True),
+                nn.Sigmoid()
+                )
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = self.conv_du(y)
+        return x * y
+
+class Self_attentionLayer(nn.Module):
+    def __init__(self, dim_in, dim_k, dim_v):   # 特征维度
+        super(Self_attentionLayer, self).__init__()
+        self.dim_in = dim_in
+        self.dim_k = dim_k
+        self.dim_v = dim_v
+        self.linear_q = nn.Linear(dim_in, dim_k, bias=False)
+        self.linear_k = nn.Linear(dim_in, dim_k, bias=False)
+        self.linear_v = nn.Linear(dim_in, dim_v, bias=False)
+        self._norm_fact = 1 / sqrt(dim_k)
+
+    def forward(self, x):
+        # x: batch, n, dim_in
+        batch, n, dim_in = x.shape
+        assert dim_in == self.dim_in
+
+        q = self.linear_q(x)  # batch, n, dim_k
+        k = self.linear_k(x)  # batch, n, dim_k
+        v = self.linear_v(x)  # batch, n, dim_v
+
+        dist = torch.bmm(q, k.transpose(1, 2)) * self._norm_fact  # batch, n, n
+        dist = torch.softmax(dist, dim=-1)  # batch, n, n
+
+
+        att = torch.bmm(dist, v)
+        return att
